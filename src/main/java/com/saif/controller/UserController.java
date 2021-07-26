7ef59886a901eeb95c199fd9e2199198b1610d76
nazmussaif-dsi <nazmussaif-dsi@dsinnovators.com>
2021-07-27 00:19:52 +0600

    updates UserController

--- src/main/java/com/saif/controller/UserController.java
+++ src/main/java/com/saif/controller/UserController.java
@@ -1,7 +1,44 @@
 package com.saif.controller;
 
+import com.saif.model.User;
+import com.saif.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/users")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public String showAllUsers(Model model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "users/user_list";
+    }
+
+    @GetMapping("add")
+    public String showUserForm(Model model){
+        model.addAttribute("user", new User());
+        return "userForm";
+    }
+
+    @GetMapping("{id}")
+    public String showUser(@PathVariable("id") Long id, Model model){
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+
+        return "user_details";
+    }
+
+    @PostMapping("add")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
 }
