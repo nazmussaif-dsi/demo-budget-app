2e1e32f5ca9f3340a629a78e72708f223aa5dc6b
nazmussaif-dsi <nazmussaif-dsi@dsinnovators.com>
2021-07-27 00:22:18 +0600

    [WIP] updates User service

--- src/main/java/com/saif/service/UserService.java
+++ src/main/java/com/saif/service/UserService.java
@@ -1,4 +1,26 @@
 package com.saif.service;
 
+import com.saif.model.User;
+import com.saif.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public User saveUser(User user){
+        return userRepository.saveUser(user);
+    }
+
+    public User getUserById(Long id){
+        return userRepository.getUserById(id);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.getAllUsers();
+    }
 }
