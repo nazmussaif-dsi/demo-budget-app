7b56b32a37deefbf344fd4a8a5fb63b7190fa9b0
nazmussaif-dsi <nazmussaif-dsi@dsinnovators.com>
2021-07-27 19:51:58 +0600

    updates UserRepository

--- src/main/java/com/saif/repository/UserRepository.java
+++ src/main/java/com/saif/repository/UserRepository.java
@@ -1,4 +1,28 @@
 package com.saif.repository;
 
+import com.saif.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepository {
+    private static final List<User> userList = new ArrayList<>();
+
+    public User saveUser(User user){
+        userList.add(user);
+        return user;
+    }
+
+    public User getUserById(Long id){
+        for(User user : userList){
+            if(user.getId() == id){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public List<User> getAllUsers(){
+        return userList;
+    }
 }
