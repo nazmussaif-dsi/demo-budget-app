31f4b2ab73d8f6cc1882173e57263844a6c3644a
nazmussaif-dsi <nazmussaif-dsi@dsinnovators.com>
2021-08-06 11:17:31 +0600

    [add/update] adds user roles and password

--- src/main/java/com/saif/model/UserRoles.java
+++ src/main/java/com/saif/model/UserRoles.java
@@ -5,21 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
-public class User {
-
+public class UserRoles {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   private String name;
-  private String password;
-  private String email;
 }
