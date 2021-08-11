07215e99c13964a792a838c7a42f290009abbf82
nazmussaif-dsi <nazmussaif-dsi@dsinnovators.com>
2021-08-11 19:31:55 +0600

    [update] optimises HomeController

--- src/main/java/com/saif/controller/HomeController.java
+++ src/main/java/com/saif/controller/HomeController.java
@@ -1,12 +1,8 @@
 package com.saif.controller;
 
-import com.saif.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
